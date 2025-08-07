f6d630a84a44ee81896824f92fc2c18f52314943
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-08-07 04:46:17 -0700

    .

--- src/core/analysis/static_analysis.py
+++ src/core/analysis/static_analysis.py
@@ -6,3 +6,21 @@ from src.utils.logger import logger
 from src.utils.file_utils import find_files
 from src.data_models.analysis import AnalysisResult, FileAnalysis
 
+
+class StaticAnalyzer:
+    TOOLS = {
+        "py": ["bandit", "pylint"],
+        "js": ["eslint"],
+        "yaml": ["yamllint"],
+        "yml": ["yamllint"]
+    }
+
+    def analyze_repository(self, repo_path):
+        results = []
+        file_types = set(self.TOOLS.keys())
+
+        for ext in file_types:
+            files = find_files(repo_path, f"*.{ext}")
+            for file_path in files:
+                relative_path = str(Path(file_path).relative_to(repo_path))
+                file_result = self.analyze_file()
