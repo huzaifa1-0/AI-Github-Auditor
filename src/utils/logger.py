4f36dad14947a2a01cf2324c725a0af240bde516
Huzaifa Rashid <huzaifaras10@gmail.com>
2025-08-07 05:52:58 -0700

    .

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,3 +4,17 @@ from loguru import logger
 
 
 
+def configure_logger():
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        level="INFO"
+    )
+    logger.add(
+        "logs/audit.log",
+        rotation="10 MB",
+        retention="30 days",
+        level="DEBUG"
+    )
+    return logger
